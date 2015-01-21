a8a4fdeca7e2ddcd3f101aff48022a9b7ec6977c
Anthony Scemama <scemama@irsamc.ups-tlse.fr>
2015-01-21 23:43:25 +0100

    Install gfortran in travis CI

---
+++ travis_ci/install_zmq.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+#
+# Tests the build distribution
+
+
+ZMQ_TGZ="zeromq-4.0.5.tar.gz"
+
+export C_INCLUDE_PATH=${C_INCLUDE_PATH}:./
+
+# Download ZeroMQ library
+# =======================
+
+if [[ ! -f ${ZMQ_TGZ} ]]
+then
+   wget "http://download.zeromq.org/"${ZMQ_TGZ}
+   if [[ $? -ne 0 ]]
+   then
+      echo "Unable to download ${ZMQ_TGZ}"
+      exit 1
+   fi
+fi
+
+# Install ZeroMQ library
+# ======================
+
+mkdir lib
+
+tar -zxf ${ZMQ_TGZ}
+pushd ${ZMQ_TGZ%.tar.gz}
+./configure || exit 1
+make || exit 1
+#cp src/.libs/libzmq.a ../lib
+cp src/.libs/libzmq.so ../lib/libzmq.so.4
+cp include/{zmq.h,zmq_utils.h} ../lib
+popd
+pushd lib
+ln -s libzmq.so.4 libzmq.so
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$PWD
+export LIBRARY_PATH=$LIBRARY_PATH:$PWD
+export ZMQ_H=$PWD/zmq.h
+popd
+
